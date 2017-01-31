dd20fbb4db8b5e8242c61f1f7f1afdd74a41bcab
shkwsk <ren7link@gmail.com>
2017-02-01 00:04:04 +0900

    add: server.go

---
+++ server.go
@@ -0,0 +1,14 @@
+package main
+
+import ("fmt"
+	"net/http"
+)
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", r.URL.Path[1:])
+}
+
+func main() {
+	http.HandleFunc("/", viewHandler)
+	http.ListenAndServe(":8080", nil)
+}
